c1f19b298b39007f29d843fe62b31d1b45ac92a1
Mukul21041998 <30376526+Mukul21041998@users.noreply.github.com>
2020-05-13 20:18:53 +0530

    2020_05_13

---
+++ DrinkAndDelightModule/Backend/display-distributor-details/src/test/java/com/cg/displaydistributordetails/DisplayDistributorDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.displaydistributordetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayDistributorDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ DrinkAndDelightModule/Backend/netflix-eureka-server/src/main/java/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+/*
+ * public class Snippet { insert into movierating_tbl values (1003,3);
+ * 
+ * }
+ * 
+ */
